62e19d7bdafaf8b1623e7806a2611173dca25d65
Pedro <pedrohlucena413@gmail.com>
2024-09-10 23:02:46 -0300

    add colors token doc

---
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -0,0 +1,31 @@
+import { colors } from '@ignite-ui/tokens'
+import { getContrast } from 'polished'
+
+export function ColorsGrid() {
+  return Object.entries(colors).map(([key, color]) => {
+    return (
+      <div
+        key={key}
+        style={{
+          backgroundColor: color,
+          padding: '2rem',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            fontFamily: 'monospace',
+            color:
+              getContrast(color, colors.white) <= 3.5
+                ? colors.black
+                : colors.white,
+          }}
+        >
+          <strong>${key}</strong>
+          <span>{color}</span>
+        </div>
+      </div>
+    )
+  })
+}
