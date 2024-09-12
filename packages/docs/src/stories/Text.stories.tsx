8565d385c9dea4d32e2dda234c3263caa344a92c
Pedro <pedrohlucena413@gmail.com>
2024-09-12 18:55:34 -0300

    add text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio porro neque provident, quaerat tenetur magni perspiciatis hic ab aut, delectus velit exercitationem debitis, ipsam voluptate? Delectus, neque sit? Architecto, aut!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
