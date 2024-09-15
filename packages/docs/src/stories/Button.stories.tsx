7bd9357cec36a60684e1d21a85d3a5bf375102e2
Pedro <pedrohlucena413@gmail.com>
2024-09-15 20:32:02 -0300

    add changesets

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Button, ButtonProps } from '@ignite-ui/react'
+import { Button, ButtonProps } from '@pedrohlucena/react'
 import { ArrowRight } from '@phosphor-icons/react'
 
 export default {
---
+++ packages/react/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @pedrohlucena/react
+
+## 2.0.0
+
+### Major Changes
+
+- Add displayName to all the components
